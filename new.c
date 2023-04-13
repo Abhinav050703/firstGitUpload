f625e7200fa4ca4162ff355d9b52cbf934554855
abhinav050703 <seemabhinavmrt@5703>
2023-04-13 13:13:32 +0530

    pattern

---
+++ new.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int m=1;
+    printf("enter height: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            printf("#");
+        }
+        m=m+1;
+        printf("\n");
+    }
+}
